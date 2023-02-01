bac0cc2b83d4f43ea3074ba6580dc51b28ae8d3f
Philip Oedi <OediP@rki.de>
2023-02-01 10:34:42 +0100

    ets added

--- daki_tools/forecast.py
+++ daki_tools/forecast.py
@@ -1,12 +1,49 @@
-#
-# from statsmodels.tsa.exponential_smoothing.ets import ETSModel
-#
-#
-#
-# def forcast(data):
-#     model = ETSModel(data.value.values)
-#     fit = model.fit(maxiter=10000)
-#     a = fit.simulate(anchor="end", nsimulations=14, repetitions=100)
-#     k = pd.DataFrame(a)
-#     k["n"] = k.index
-#     pd.melt(k, id_vars=["n"])
+# forecast.py
+# Philip Oedi
+# 2023-02-01
+
+from abc import ABC, abstractmethod
+
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.exponential_smoothing.ets import ETSModel
+
+
+class BaseForecast(ABC):
+    @abstractmethod
+    def fit(self, data):
+        pass
+
+    @abstractmethod
+    def predict(self, fh, n):
+        pass
+
+
+class Ets(BaseForecast):
+    def __init__(self, **kwargs):
+        self.model = ETSModel(np.empty(1))
+        self.model_results = None
+        if kwargs:
+            for p, v in kwargs.items():
+                setattr(self.model, p, v)
+
+    def fit(self, data):
+        """
+
+        :param data:
+        :return:
+        """
+        self.model.endog = data.value.values
+        self.model_results = self.model.fit(maxiter=10000)
+
+    def predict(self, fh, n):
+        y_pred = self.model_results.simulate(
+            anchor="end", nsimulations=fh, repetitions=n
+        )
+        y_pred = pd.DataFrame(y_pred)
+        y_pred["n"] = y_pred.index
+        return pd.melt(y_pred, id_vars=["n"])
+
+    def fit_predict(self, data, fh, n):
+        self.fit(data)
+        return self.predict(fh, n)
---
+++ tests/test_forecast.py
@@ -0,0 +1,51 @@
+# test_foreacast.py
+# Philip Oedi
+# 2023-02-01
+
+import os
+
+import pytest
+from statsmodels.tsa.exponential_smoothing.ets import ETSResultsWrapper
+
+from daki_tools import forecast, load
+
+
+@pytest.fixture
+def csv_data():
+    file = os.path.join("data", "test.csv")
+    data = load.load(file)
+    return load.subset(data, 1001)
+
+
+def test_ets_fit(csv_data):
+    """
+
+    :param csv_data:
+    :return:
+    """
+    model = forecast.Ets()
+    model.fit(csv_data)
+    assert isinstance(model.model_results, ETSResultsWrapper)
+
+
+def test_ets_predict(csv_data):
+    """
+
+    :param csv_data:
+    :return:
+    """
+    model = forecast.Ets()
+    model.fit(csv_data)
+    y_pred = model.predict(fh=14, n=10)
+    assert y_pred.shape == (14 * 10, 3)
+
+
+def test_ets_init():
+    model = forecast.Ets(error="add")
+    assert model.model.error == "add"
+
+
+def test_ets_fit_predict(csv_data):
+    ets_aa = forecast.Ets(error="add", trend="add")
+    y_pred = ets_aa.fit_predict(csv_data, 14, 10)
+    assert y_pred.shape == (14 * 10, 3)
